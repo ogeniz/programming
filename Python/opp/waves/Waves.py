2e0bd6896e9243e444694c6af9b9f052509471a4
Ogeniz Façanha Costa <ogeniz@gmail.com>
2015-04-08 08:40:22 -0300

    new approach of waves.py

--- Python/opp/waves/Waves.py
+++ Python/opp/waves/Waves.py
@@ -5,8 +5,7 @@ class Media: # media class definition
     def __init__(self,epsilon = float(1.0),mu = float(1.0),sigma = float(0.0)):
         self.__epsilon = epsilon * (10**(-9)/(36*math.pi)) # F/m
         self.__mu = mu * (4*math.pi*10**(-7)) # H/m
-        self.__sigma = sigma # Siemens
-        self.__impedance = math.sqrt(self.__mu/self.__epsilon)
+        self.__sigma = sigma # Siemens        
     def setEpsilon(self,value):
         self.__epsilon = value * (10**(-9)/(36*math.pi))
     def setMu(self,value):
@@ -19,21 +18,19 @@ class Media: # media class definition
         return self.__mu
     def getSigma(self):
         return self.__sigma
-    def getImpedance(self):
-        return self.__impedance
     def printMedia(self):
-        print("epsilon (F/m) = ",self.getEpsilon(),"mu (H/m) =",self.getMu(),
-              "sigma (S)=",self.getSigma(),"impedance (ohms) =",self.getImpedance())
+        print("epsilon (F/m) = ",self.getEpsilon(),"mu (H/m) =",self.getMu(),"sigma (S)=",self.getSigma())
 
 class Wave: # wave class definition
-    def __init__(self,freq = float(0.0),mod = float(1.0),phs = float(0.0)):
-        self.__module = mod
-        self.__phase = phs*(math.pi/180) # rad
-        self.__frequency = freq # Hz
+    def __init__(self,module = float(1.0),frequency = float(0.0),phase = float(0.0)):
+        self.__module = module
+        self.__phase = phase*(math.pi/180) # rad
+        self.__frequency = 2*math.pi*frequency # rad/s
+    # end of default constructor
     def setModule(self,value):
         self.__module = value
     def setFrequency(self,value):
-        self.__frequency = value
+        self.__frequency = 2*math.pi*value # rad/s
     def setPhase(self,value):
         self.__phase = value*(math.pi/180) # rad
     def getFrequency(self):
@@ -45,12 +42,32 @@ class Wave: # wave class definition
     def printWave(self):
         print("frequency (rad/s) =",self.getFrequency(),"module =",self.getModule(),"phase (rad) =",self.getPhase())
 
-class ElectroMagneticWave(Wave): # electromagnetic wave definition
-    def __init__(self,e = float(1.0),m = float(1.0),s = float(1.0),mod = float(1.0),freq = float(1.0),phs = float(0.0)):
-        self.__Media = Media(epsilon = e,mu = m,sigma = s)
-        self.__electricfield = Wave(module = mod,frequency = freq,phase = phs)
-        self.__magneticfield = Wave(module = (mod/self.__Media.getImpedance()),frequency = freq,phase = phs)
-        #wave.setModule(self.__electricfield.getModule()/media.getImpedance())
+class ElectroMagneticWave(): # electromagnetic wave definition
+    def __init__(self,module = float(1.0),frequency = float(1.0),phase = float(0.0),epsilon = float(1.0),mu = float(1.0),sigma = float(0.0)):
+        self.__Media = Media(epsilon = epsilon,mu = mu,sigma = sigma)
+        self.__impedance = cmath.sqrt(1j*frequency*self.__Media.getMu()/(self.__Media.getSigma() + 1j*frequency*self.__Media.getEpsilon()))
+        self.__electricfield = Wave(module = module,frequency = frequency,phase = phase)
+        self.__magneticfield = Wave(module = (module/abs(self.__impedance)),frequency = frequency,phase = phase + cmath.phase(self.__impedance))
+        # end of default constructor
+    def updateMagnecticField(self): # if the media change, changes the magnetic field
+        self.__impedance = cmath.sqrt(1j*self.__electricfield.getFrequency()*self.__Media.getMu()/(self.__Media.getSigma() + 1j*self.__electricfield.getFrequency()*self.__Media.getEpsilon()))
+        self.__magneticfield.setPhase(self.__electricfield.getPhase())
+        if (self.__impedance.imag == 0):
+            self.__magneticfield.setModule(self.__electricfield.getModule()/self.__impedance)
+        else:
+            self.__magneticfield.setModule(self.__electricfield.getModule()/abs(self.__impedance))
+            self.__magneticfield.setPhase(self.__electricfield.getPhase() + cmath.phase(self.__impedance))
+    def setEpsilon(self,value):
+        self.__Media.setEpsilon(value)
+        self.updateMagnecticField()
+    def setMu(self,value):
+        self.__Media.setMu(value)
+        self.updateMagnecticField()
+    def setSigma(self,value):
+        self.__Media.setSigma(value)
+        self.updateMagnecticField()
+    def getMedia(self):
+        self.__Media.printMedia()
     def getElectricField(self):
         print("Electric Field ->","module =",self.__electricfield.getModule(),
               "frequency =",self.__electricfield.getFrequency(),
@@ -59,3 +76,12 @@ class ElectroMagneticWave(Wave): # electromagnetic wave definition
         print("Magnectic Field ->","module =",self.__magneticfield.getModule(),
               "frequency =",self.__magneticfield.getFrequency(),
               "phase =",self.__magneticfield.getPhase())
+
+W = ElectroMagneticWave(module = 10.0,frequency = 10**3,epsilon = 54.0,sigma = 3.9) # 10V e 100Hz
+W.getMedia()
+W.getElectricField()
+W.getMagneticField()
+W.setSigma(0.0)
+W.getMedia()
+W.getElectricField()
+W.getMagneticField()
