c401b3ba213de9993087c0d0cb0d9a8b0459e03f
Ogeniz Façanha Costa <ogeniz@gmail.com>
2015-05-07 18:24:03 -0300

    XEmacs setup files

---
+++ configs/xemacs/custom.el
@@ -0,0 +1,32 @@
+(custom-set-variables
+ '(backup-by-copying t)
+ '(buffers-menu-format-buffer-line-function 'slow-format-buffers-menu-line)
+ '(buffers-tab-switch-to-buffer-function 'switch-to-buffer)
+ '(c-default-style '((c-mode . "ellemtel")
+  (java-mode . "java")
+  (other . "gnu"))
+)
+ '(c-echo-syntactic-information-p t)
+ '(c-enable-xemacs-performance-kludge-p t)
+ '(complex-buffers-menu-p t)
+ '(confirm-kill-emacs nil)
+ '(custom-menu-sort-alphabetically t)
+ '(delete-selection-mode t)
+ '(display-time-day-and-date t)
+ '(font-lock-mode t nil (font-lock))
+ '(icomplete-mode t nil (icomplete))
+ '(make-backup-files nil)
+ '(menu-accelerator-enabled 'menu-force t))
+(custom-set-faces
+ '(default ((t (:size "18pt" :family "Lucidabright"))) t))
+
+
+
+
+
+
+
+
+
+
+
---
+++ configs/xemacs/init.el
@@ -0,0 +1,3 @@
+(global-set-key(kbd "<DEL>") 'delete-primary-selection)
+(global-set-key(kbd "<f9>") 'kill-emacs)
+(global-set-key(kbd "<f10>") 'accelerate-menu)
